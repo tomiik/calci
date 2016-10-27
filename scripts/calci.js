e97d37440626d56a635d8f66e74ecfce2dc9fd01
tomii <g244103@outlook.com>
2016-10-27 11:13:43 +0530

    Modularizing the script

--- scripts/calci.js
+++ scripts/calci.js
@@ -1,43 +1,44 @@
-function handleInput(input){
-  $("#preview").html($("#preview").html() + input);
-}
-
-function handleDelete(){
-  $("#preview").html($("#preview").html().slice(0,-1));
-}
-
-function handleDeleteAll(){
-  $("#preview").html('');
-  $("#result").html('');
-}
-
-function evaluateResult() {
-  $("#result").html(eval($("#preview").html()));
-}
-
-$(document).ready(function(){
-  $('#calculator .input').click(function(){
-    var input;
-    if(this.dataset.keyType == "digit"){
-      handleInput(this.dataset.digit);
-    }else if (this.dataset.keyType == "operator"){
-      handleInput(this.dataset.operator);
-    }else if (this.dataset.keyType == "delete"){
-      handleDelete();
-    }else if (this.dataset.keyType == "equals"){
-      evaluateDelete();
-    }
-  });
-
-  $("#calculator #delete").dblclick(function(){
-    handleDeleteAll();
-  });
-
-  ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'].forEach(function(digit){
-    $(document).bind('keyup',digit,function(){
-      handleInput(digit);
+var Calci = {
+  init: function() {
+    $('#calculator .input').click(function(){
+      var input;
+      if(this.dataset.keyType == "digit"){
+        Calci.handleInput(this.dataset.digit);
+      }else if (this.dataset.keyType == "operator"){
+        Calci.handleInput(this.dataset.operator);
+      }else if (this.dataset.keyType == "delete"){
+        Calci.handleDelete();
+      }else if (this.dataset.keyType == "equals"){
+        Calci.evaluateDelete();
+      }
     });
 
-  });
+    $("#calculator #delete").dblclick(function(){
+      Calci.handleDeleteAll();
+    });
 
+    ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'].forEach(function(digit){
+      $(document).bind('keyup',digit,function(){
+        Calci.handleInput(digit);
+      });
+    });
+  },
+  handleInput: function(input){
+    $("#preview").html($("#preview").html() + input);
+  },
+  handleDelete: function(){
+    $("#preview").html($("#preview").html().slice(0,-1));
+  },
+  handleDeleteAll: function(){
+    $("#preview").html('');
+    $("#result").html('');
+  },
+  evaluateResult: function() {
+    $("#result").html(eval($("#preview").html()));
+  }
+}
+
+
+$(document).ready(function(){
+  Calci.init();
 });
